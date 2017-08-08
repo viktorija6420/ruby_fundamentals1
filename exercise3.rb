9c31d21c5bed5a07212067271e8659d8e906c94d
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-08-08 18:21:44 -0400

    Made it through this exercise alive

--- exercise3.rb
+++ exercise3.rb
@@ -1,2 +1,13 @@
 
 puts "What is your name?"
+
+Viktorija_Gjorgjievska = gets.chomp
+
+puts "Hello, #{Viktorija_Gjorgjievska}"
+
+
+puts "How old are you?"
+age = gets.chomp
+age = 27
+current_year = 2017
+print "year of birth #{current_year - age}"
